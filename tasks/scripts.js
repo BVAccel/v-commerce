20bfe7a915942735e695f99311a900d31616b78a
dlerm <younglerman@gmail.com>
2019-11-01 14:26:40 -0700

    JS Bundles Update

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,31 +1,29 @@
-
-var gulp       = require('gulp');
-var webpack    = require('webpack-stream');
+var gulp = require('gulp');
+var cache = require('gulp-cached');
+var webpack = require('webpack-stream');
 
 var webpackDev = require('../webpack.dev.js');
 var webpackProd = require('../webpack.prod.js');
 
-var scriptsPath = 'src/scripts/bvaccel.js';
+var scriptsPaths = ['src/scripts/templates/*.js', 'src/scripts/layout/*.js'];
 var scriptsDest = 'dist/assets';
 
-gulp.task('scripts:dev', function () {
-  return gulp.src(scriptsPath)
+gulp.task('scripts:dev', function() {
+  return gulp
+    .src(scriptsPaths)
     .pipe(webpack(webpackDev))
+    .pipe(cache('scripts'))
     .pipe(gulp.dest(scriptsDest));
 });
 
-gulp.task('scripts:prod', function () {
-  return gulp.src(scriptsPath)
+gulp.task('scripts:prod', function() {
+  return gulp
+    .src(scriptsPaths)
     .pipe(webpack(webpackProd))
     .pipe(gulp.dest(scriptsDest));
 });
 
-gulp.task('scripts:watch', function (done) {
-  gulp.watch([
-    'src/scripts/**/*',
-    'src/styles/boilerplate.scss',
-    'src/styles/variables.scss',
-    'src/styles/tools/*.scss',
-  ], gulp.series('scripts:dev'));
+gulp.task('scripts:watch', function(done) {
+  gulp.watch(['src/scripts/**/*', 'src/styles/boilerplate.scss', 'src/styles/variables.scss', 'src/styles/tools/*.scss'], gulp.series('scripts:dev'));
   done();
 });

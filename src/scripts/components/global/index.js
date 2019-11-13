1f5f6171f945ae70527ab9990f106c06660c11b5
dlerm <younglerman@gmail.com>
2019-11-12 17:19:14 -0800

    Add theme section dto

--- src/scripts/components/global/index.js
+++ src/scripts/components/global/index.js
@@ -1,9 +1,11 @@
 import GlobalNav from 'scripts/components/navigation/GlobalNav.vue';
 import TestButton from 'scripts/components/global/buttons/TestButton.vue';
 import LinkListDto from 'scripts/components/global/dto/link-list-dto.vue';
+import SectionDto from 'scripts/components/global/dto/section-dto.vue';
 
 export default {
   GlobalNav,
   TestButton,
-  LinkListDto
+  LinkListDto,
+  SectionDto,
 };
--- src/scripts/store/dispatch.js
+++ src/scripts/store/dispatch.js
@@ -1,16 +1,17 @@
 export const CartDispatch = {
-    addItem : 'cart/addItem',
-    init : 'cart/init',
-    updateItem : 'cart/updateItem',
-    removeItem : 'cart/removeItem',
-    openSidecart : 'cart/openSidecart',
-    closeSidecart : 'cart/closeSidecart'
-}
+  addItem: 'cart/addItem',
+  init: 'cart/init',
+  updateItem: 'cart/updateItem',
+  removeItem: 'cart/removeItem',
+  openSidecart: 'cart/openSidecart',
+  closeSidecart: 'cart/closeSidecart',
+};
 
 export const CollectionDispatch = {
-    setCollection : 'collections/setCollection'
-}
+  setCollection: 'collections/setCollection',
+};
 
 export const DtoDispatch = {
-    setLinkList : 'dto/setLinkLists'
-}
+  setLinkList: 'dto/setLinkLists',
+  setSection: 'dto/setSection',
+};
